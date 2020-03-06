739738844f258e9d4464dfdcc9496f2d3702bf27
Sebastian Park <SebPark03@gmail.com>
2020-03-06 10:39:32 -0500

    Update style.dart

---
+++ my_milton/lib/theme/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData mainTheme = ThemeData(
+  canvasColor: const Color(0xfff0f0f0),
+  brightness: Brightness.light,
+  primarySwatch: Colors.blue,
+  accentColor: Colors.orangeAccent,
+);
+
+ThemeData darkTheme = ThemeData.dark();
